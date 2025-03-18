b660a27f1b33d34bc1e3ced379b0f123ecaa426c
sashisridhar <sashi.sridhar@srpnet.com>
2025-03-18 06:54:16 -0700

    Update streamlit_app.py
    Added import for API call and tested it

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
 
+st.text(smoothiefroot_response.json())
 helpful_links = [
     "https://docs.streamlit.io",
     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
