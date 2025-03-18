0122c5ae77abaef9c61a98aeca5c132da3a218f7
sashisridhar <sashi.sridhar@srpnet.com>
2025-03-18 07:10:53 -0700

    Update streamlit_app.py
    Commented other code

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#import requests
+#smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 
 
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+#st.text(smoothiefroot_response.json())
+#sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 helpful_links = [
     "https://docs.streamlit.io",
     "https://docs.snowflake.com/en/developer-guide/streamlit/about-streamlit",
