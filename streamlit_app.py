798ea6c380ce3903fbf938316a1276080e55113c
sashisridhar <sashi.sridhar@srpnet.com>
2025-03-19 07:09:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,9 +30,7 @@ session = cnx.session()
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width = True)
 #st.stop()
-ingredients_list = st.multiselect('Choose up to 5 ingredients:'
-            , my_dataframe
-            , max_selections = 5)
+ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections = 5)
 if ingredients_list:
    
     ingredients_string = ''
